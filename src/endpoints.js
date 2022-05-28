bf308b9087c355e6517806207e96afcf8bf94b6d
Gabriel Firmino <firminogabriel89@gmail.com>
2022-05-28 13:01:48 -0300

    SequenciaPar e fatorial

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,25 +1,69 @@
+
 import { Router } from "express"
-
-import { Semaforo, DiaSemana } from "./services.js"
-
+import {Semaforo, DiaSemana, Fatoral, SequenciaPar} from "./services.js"
 const server = Router();
 
-server.get('/Semaforo/:cor', (req, resp ) => {
-    const cor = req.params.cor
 
-    const x = Semaforo(cor)
-    resp.send ({
-        Semaforo: x
-    })
+server.get('/dia4/semaforo/:cor', (req, resp) => {
+    try {
+        const {cor} = req.params;
+        const x = Semaforo(cor)
+        resp.send ({
+            cor:x
+        })
+    }
+
+    catch (err) {
+        resp.send ({
+            erro : err.message
+        })
+    }
 })
 
-server.get('/DiaSemana', (req, resp) => {
-    const dia = req.query.dia;
-    const x = DiaSemana(dia);
+server.get ('/dia4/diasemana', (req, resp)=>{
+    try {
+        const {x} = req.query;
+        const j = DiaSemana(x)
+        resp.send({
+            dia : j
+        })
+    }
+    catch (err) {
+        resp.send ({
+            erro : err.message
+        })
+    }
+})
 
-    resp.send({
-        dia: x
-    })
+server.post('/dia4/fatorial' , (req, resp)=>{
+    try {
+        const x = Number(req.query.x)
+        const j = Fatoral(x)
+        resp.send({
+            FAT : j
+        })
+    }
+    catch (err) {
+        resp.send({
+            erro : err.message
+        })
+    }
+})
+
+
+server.post('/dia4/sequenciapar', (req, resp) => {
+    try {
+        const a = req.body.limite
+        const x = SequenciaPar(a)
+        resp.send ({
+            limite : x
+        })
+    }
+    catch (err) {
+        resp.send({
+            erro: err.message
+        })
+    }
 })
 
 export default server;
--- src/services.js
+++ src/services.js
@@ -1,48 +1,63 @@
-export function Semaforo(cor){
-    let msg = "";
-    if(cor == "verde")
-    {
-        msg = "Pode atravessar";
+export function Semaforo(cor) {
+    
+    let msg = ''
+    
+    if (cor == "verde") {
+        msg = 'Pode Passar'
     }
-
-    else if(cor == "vermelho")
-    {
-        msg = "Não atravesse";
+    else if (cor == "vermelho") {
+    msg = 'Aguardar'
     }
-
     else{
-        msg = "Invalido";
+        msg= 'Inválido'
     }
-
     return msg;
 }
 
-export function DiaSemana(dia){
-    let nome = "";
+
+export function DiaSemana (dia) {
+    let msg = ""
     if(dia == 0){
-        nome = "domingo";
+        msg = "Domingo"
     }
-    else if(dia == 1){
-        nome = "segunda";
+    else if (dia == 1){
+        msg = "Segunda"
     }
-    else if(dia == 2){
-        nome = "terça";
+    else if (dia == 2){
+        msg= "Terça"
     }
-    else if(dia == 3){
-        nome = "quarta";
+    else if (dia == 3){
+        msg= "Quarta"
     }
-    else if(dia == 4){
-        nome = "quinta";
+    else if (dia == 4){
+        msg= "Quinta"
     }
-    else if(dia == 5){
-        nome = "sexta";
+    else if (dia == 5){
+        msg = "Sexta"
     }
-    else if(dia == 6){
-        nome = "sábado";
+    else if (dia == 6){
+        msg= "Sábado"
     }
-    else{
-        return "Dia invalido"
+    return msg;
+}
+
+export function Fatoral(num) {
+let a = 1; 
+for(let cont= num; cont>1;cont--){
+    a*= cont
+}
+return a;
+
+}
+
+export function SequenciaPar (num) {
+    let a = [];
+    let b=0
+
+
+    for (let i = 0; i <= num; i++) {
+        a[i]= b * 2
+        b++
     }
-    
-    return nome;
+        return a;
 }
